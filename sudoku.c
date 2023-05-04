4f5969dd030a628f5584d21ca51d71d2d88f50fb
Replit user <>
2023-05-04 02:22:17 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -87,18 +87,25 @@ List* get_adj_nodes(Node* n){
     List* list=createList();
     
     for(int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
-            if(n->sudo[i][j] == 0){  
-                for(int k=1; k<=9; k++){
-                    Node* new_node = copy(n);
-                    new_node->sudo[i][j] = k;
-                    if(is_valid(new_node)){
-                        pushBack(list, new_node);
-                    }else{
-                        free(new_node);
-                    }
-                }
-                return list;
+      
+    for(int j=0; j<9; j++){
+      
+        if(n->sudo[i][j] == 0){  
+          
+            for(int k=1; k<=9; k++){
+              
+              Node* new_node = copy(n);
+              new_node->sudo[i][j] = k;
+              if(is_valid(new_node)){
+              pushBack(list, new_node);
+             }else{
+              free(new_node);
+            }
+              
+            }
+          
+        return list;
+          
             }
         }
     }
@@ -108,7 +115,14 @@ List* get_adj_nodes(Node* n){
 
 
 int is_final(Node* n){
-    return 0;
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            if(n->sudo[i][j] == 0){
+                return 0;
+            }
+        }
+    }
+    return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
